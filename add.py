47ae9bed31bbe365d2cce76715cf0647946c682f
billayamini18 <billayamini18@gmail.com>
2025-10-06 21:53:20 +0530

    add.py

--- add.py
+++ add.py
@@ -1,7 +1,5 @@
 def add_employee(employees):
-    # Adds a new employee after validating input using conditionals.
     print("----Add new employee----")
-    # Input and Validate Employee ID
     while True:
         id_input=input("Enter employee_id")
         if not id_input.isdigit():
@@ -13,18 +11,16 @@ def add_employee(employees):
             continue
         else:
             break            
-    # Input other details
     name=input("Enter name")
     department=input("Enter Department")
     role=input("Enter Role")
     while True:
         try:
             salary = int(input("Enter Salary: "))
-            break  # This line is only reached if int() succeeds
+            break  
         except ValueError:
-            # This code runs if a ValueError occurs
+    
             print("Invalid input. Please enter a number using only digits (0-9).")
-    # Create the employee dictionary
     employee={
         "id":emp_id,
         "name":name,
@@ -32,6 +28,6 @@ def add_employee(employees):
         "role":role,
         "salary":salary
     }
-    #adds to employees
     employees.append(employee)
+
     print(f"Employee '{name}' added successfully!!")
